e549b842779263ca790ab9cfd77e4cc4d8bfc778
camilapadalino <camilappadalino@gmail.com>
2023-11-24 16:11:54 -0300

    versão final

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ import time
 from plyer import notification
 import schedule
 import threading
-import random
-import time
-from paho.mqtt import client as mqtt_client
 
-broker = '46.17.108.113'
-port = 1883
-topic = "/TEF/hosp230/cmd"
-client_id = f'publish-{random.randint(0, 1000)}'
+
+
 
 # Leia o README antes de rodar o código, pois há informações importantes!!!!!!
 
@@ -129,7 +124,7 @@ def notificacoes_thread():
 
 
 # Aqui é onde o enfermeiro(a) adiciona o medicamento pro paciente desejado
-def inserirMedicamento(client):
+def inserirMedicamento():
     global paciente_atual
     cpf = input("Digite o CPF do paciente: ")
     time.sleep(1)
@@ -181,14 +176,6 @@ def inserirMedicamento(client):
                 print(f'Medicamento inserido com sucesso para o paciente com CPF {cpf}')
                 time.sleep(2)
 
-                msg = f"Hora de tomar o remédio {paciente_atual['nome']}!"
-                #msg = f"On"
-                result = client.publish(topic, msg)
-                status = result.rc
-                if status == 0:
-                    print(f"Send `{msg}` to topic `{topic}` via MQTT")
-                else:
-                    print(f"Failed to send message to topic {topic} via MQTT")
 
             
 
@@ -375,39 +362,6 @@ def excluirPaciente():
     with open('pacientes.json', 'w', encoding='utf-8') as arquivo:
         json.dump(pacientes, arquivo, indent=4, ensure_ascii=False)
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id)
-    # client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-def publish(client):
-    msg_count = 1
-    while True:
-        time.sleep(1)
-        msg = f"On"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
-        if msg_count > 5:
-            break
-
-def run(client):
-    client.loop_start()
-    publish(client)
-    client.loop_stop()
 
 
 
@@ -447,7 +401,6 @@ if EnfermeiroLogado == True:
     notificacoes_thread = threading.Thread(target=notificacoes_thread)
     notificacoes_thread.start()
     
-    client = connect_mqtt()
 
     while True:
         opcao = menuOpcoes()
@@ -456,7 +409,7 @@ if EnfermeiroLogado == True:
             cadastro()
         elif opcao == 2:
             time.sleep(1)
-            inserirMedicamento(client)
+            inserirMedicamento()
         elif opcao == 3:
             time.sleep(1)
             excluirMedicamento()
